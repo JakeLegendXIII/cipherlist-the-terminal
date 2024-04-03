82e068f51a78506b961be6b35d8dc963d1690bf1
Jacob Kirkbride <jakelegendxiii@gmail.com>
2024-04-02 22:29:11 -0400

    Pillbox and letterboxing is working

--- CipherListTerminal/CipherListTerminal/MainGame.cs
+++ CipherListTerminal/CipherListTerminal/MainGame.cs
@@ -11,9 +11,11 @@ namespace CipherListTerminal
 		private GraphicsDeviceManager _graphics;
 		private SpriteBatch _spriteBatch;
 		private RenderTarget2D _renderTarget;
+		private Rectangle _renderDestination;
 
 		private int _nativeWidth = 1280;
 		private int _nativeHeight = 800;
+		private bool _isResizing;
 
 		string[] possibleValues = { "1C", "55", "BD", "FF", "E9" };
 
@@ -34,14 +36,16 @@ namespace CipherListTerminal
 
 
 			Window.AllowUserResizing = true;
-			IsMouseVisible = true;
-		}
+			Window.ClientSizeChanged += OnClientSizeChanged;
 
+			IsMouseVisible = true;
+		}	
 		protected override void Initialize()
 		{
 			// TODO: Add your initialization logic here
 
 			base.Initialize();
+			CalculateRenderDestination();
 		}
 
 		protected override void LoadContent()
@@ -72,11 +76,11 @@ namespace CipherListTerminal
 		}
 
 		protected override void Draw(GameTime gameTime)
-		{						
-			GraphicsDevice.Clear(Color.CornflowerBlue);
-
+		{
 			// Add RenterTarget2D to allow screen resizing
-			//GraphicsDevice.SetRenderTarget(_renderTarget);
+			GraphicsDevice.SetRenderTarget(_renderTarget);
+
+			GraphicsDevice.Clear(Color.CornflowerBlue);
 
 			_spriteBatch.Begin(samplerState: SamplerState.PointClamp);
 
@@ -88,12 +92,41 @@ namespace CipherListTerminal
 			_spriteBatch.End();
 
 			GraphicsDevice.SetRenderTarget(null);
-
+			
 			_spriteBatch.Begin(samplerState: SamplerState.PointClamp);
-			_spriteBatch.Draw(_renderTarget, new Rectangle(0, 0, _nativeWidth, _nativeHeight), Color.White);
+			_spriteBatch.Draw(_renderTarget, _renderDestination, Color.White);
 			_spriteBatch.End();
 
 			base.Draw(gameTime);
+		}
+
+		private void OnClientSizeChanged(object sender, EventArgs e)
+		{
+			if (!_isResizing && Window.ClientBounds.Width > 0 && Window.ClientBounds.Height > 0)
+			{
+				_isResizing = true;
+
+				CalculateRenderDestination();
+
+				_isResizing = false;
+			}
+		}
+
+
+		private void CalculateRenderDestination()
+		{
+			Point size = GraphicsDevice.Viewport.Bounds.Size;			
+
+			float scaleX = (float)size.X / _renderTarget.Width;
+			float scaleY = (float)size.Y / _renderTarget.Height;
+
+			float scale = Math.Min(scaleX, scaleY);
+
+			_renderDestination.Width = (int)(_renderTarget.Width * scale);
+			_renderDestination.Height = (int)(_renderTarget.Height * scale);
+
+			_renderDestination.X = (size.X - _renderDestination.Width) / 2;
+			_renderDestination.Y = (size.Y - _renderDestination.Height) / 2;
 		}
 	}
 }
