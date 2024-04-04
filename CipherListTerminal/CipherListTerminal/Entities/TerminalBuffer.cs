a73f8fab9c3167f62dd641ae3c2e4831a097aea4
Jacob Kirkbride <jakelegendxiii@gmail.com>
2024-04-03 20:09:34 -0400

    Updating Text and TerminalBuffer text item

---
+++ CipherListTerminal/CipherListTerminal/Entities/TerminalBuffer.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+
+namespace CipherListTerminal.Entities
+{
+	public class TerminalBuffer : IGameEntity
+	{
+		public int DrawOrder => 100;
+
+		public void Draw(SpriteBatch spriteBatch, GameTime gameTime)
+		{
+			
+		}
+
+		public void Update(GameTime gameTime)
+		{
+			
+		}
+	}
+}
