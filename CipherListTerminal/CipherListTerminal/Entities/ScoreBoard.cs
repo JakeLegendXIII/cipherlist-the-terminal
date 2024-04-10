5559bc802124369bfc96673b82e35efd75244672
Jacob Kirkbride <jakelegendxiii@gmail.com>
2024-04-10 18:28:35 -0400

    Create scoreboard

---
+++ CipherListTerminal/CipherListTerminal/Entities/ScoreBoard.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace CipherListTerminal.Entities
+{
+	internal class ScoreBoard : IGameEntity
+	{
+		public int DrawOrder => 0;
+
+		public void Draw(SpriteBatch spriteBatch, GameTime gameTime, float scale)
+		{
+		}
+
+		public void Update(GameTime gameTime)
+		{
+		}
+	}
+}
