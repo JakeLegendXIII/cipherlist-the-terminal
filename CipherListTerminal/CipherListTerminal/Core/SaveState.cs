1e667d5787dd90e812675b75b1b3539fcb4a964e
Jacob Kirkbride <jakelegendxiii@gmail.com>
2024-04-24 13:47:53 -0400

    Add basic SaveState

---
+++ CipherListTerminal/CipherListTerminal/Core/SaveState.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace CipherListTerminal.Core
+{
+	[Serializable]
+	public class SaveState
+	{
+		public int HighScore { get; set; }
+		public DateTime HighScoreDate { get; set; }
+	}
+}
