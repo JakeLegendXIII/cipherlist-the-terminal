6e1ca33d759dd9eaac56ba982b68fd8a3067073e
Jacob Kirkbride <jakelegendxiii@gmail.com>
2024-05-19 14:04:43 -0400

    Add start of InputStateIndicator to draw on the screen

---
+++ CipherListTerminal/CipherListTerminal/Entities/InputStateIndicator.cs
@@ -0,0 +1,19 @@
+﻿using CipherListTerminal.Core;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace CipherListTerminal.Entities
+{
+	internal class InputStateIndicator : IGameEntity
+	{
+		public void Draw(SpriteBatch spriteBatch, GameTime gameTime, float scale)
+		{
+			
+		}
+
+		public void Update(GameTime gameTime, InputStates inputState)
+		{
+			
+		}
+	}
+}
