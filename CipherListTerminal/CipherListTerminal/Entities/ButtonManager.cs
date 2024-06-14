fec1a63e77a0fdc2f62f06664f51e13ab24632af
Jacob Kirkbride <jakelegendxiii@gmail.com>
2024-06-13 22:27:21 -0400

    Create base of ButtonManager

---
+++ CipherListTerminal/CipherListTerminal/Entities/ButtonManager.cs
@@ -0,0 +1,19 @@
+﻿using CipherListTerminal.Core;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace CipherListTerminal.Entities
+{
+	internal class ButtonManager : IGameEntity
+	{
+		public void Draw(SpriteBatch spriteBatch, GameTime gameTime, float scale)
+		{
+			
+		}
+
+		public void Update(GameTime gameTime, InputStates inputState)
+		{
+			
+		}
+	}
+}
