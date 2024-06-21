85e634081f35f77dfedc7132c570e8d043c14d3b
Jake Kirkbride <jakelegendxiii@gmail.com>
2024-06-20 22:30:55 -0400

    Remove weird using

--- CipherListTerminal/CipherListTerminal/Entities/ButtonManager.cs
+++ CipherListTerminal/CipherListTerminal/Entities/ButtonManager.cs
@@ -4,7 +4,6 @@ using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using System.Collections.Generic;
-using static System.Formats.Asn1.AsnWriter;
 
 namespace CipherListTerminal.Entities
 {
