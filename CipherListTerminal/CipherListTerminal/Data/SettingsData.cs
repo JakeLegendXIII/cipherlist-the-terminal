355b8b119d6fcb6a43101f3c1056dc1052e7c8d7
Jacob Kirkbride <jakelegendxiii@gmail.com>
2024-07-12 23:46:40 -0400

    Add SettingsData class

---
+++ CipherListTerminal/CipherListTerminal/Data/SettingsData.cs
@@ -0,0 +1,24 @@
+﻿namespace CipherListTerminal.Data
+{
+	public class SettingsData
+	{
+		public Highscores highScores { get; set; }
+		public Settings settings { get; set; }
+	}
+
+	public class Highscores
+	{
+		public int freePlay { get; set; }
+		public string freePlayRecordDate { get; set; }
+		public int bestOf10Timed { get; set; }
+		public string bestOf10TimedRecordDate { get; set; }
+		public int timeTrial { get; set; }
+		public string timeTrialRecordDate { get; set; }
+	}
+
+	public class Settings
+	{
+		public string displayMode { get; set; }
+		public bool crtShader { get; set; }
+	}
+}
