65a26c107e89b4c368be624a171ea248436e7f1c
Jacob Kirkbride <jakelegendxiii@gmail.com>
2024-10-21 21:48:17 -0400

    Updated

--- CipherListTerminal/CipherListTerminal/MainGame.cs
+++ CipherListTerminal/CipherListTerminal/MainGame.cs
@@ -155,7 +155,7 @@ namespace CipherListTerminal
 			_armadaFont = Content.Load<SpriteFont>("Fonts/ArmadaBold16");
 			_farawayFont = Content.Load<SpriteFont>("Fonts/Faraway16");
 
-			_backgroundTexture = Content.Load<Texture2D>("Sprites/RoughBG12");
+			_backgroundTexture = Content.Load<Texture2D>("Sprites/RoughBG13");
 			_spriteSheet = Content.Load<Texture2D>("Sprites/spritesheet");
 
 			_flickingASwitch = Content.Load<SoundEffect>("SFX/flickingaswitch");
