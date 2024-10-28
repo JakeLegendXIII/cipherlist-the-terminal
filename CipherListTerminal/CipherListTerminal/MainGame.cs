7d591b499efb0b655cc59f9b5b2db50d5b98fdc9
Jacob Kirkbride <jakelegendxiii@gmail.com>
2024-10-27 23:39:09 -0400

    Debug for Controller DisplayName added temporarily

--- CipherListTerminal/CipherListTerminal/MainGame.cs
+++ CipherListTerminal/CipherListTerminal/MainGame.cs
@@ -537,8 +537,9 @@ namespace CipherListTerminal
 			if (InputManager.IsGamePadConnected())
 			{
 				_inputStateIndicator.Draw(_spriteBatch, gameTime, _scale);
+				_spriteBatch.DrawString(_armadaFont, $" GamePadDisplayName: {InputManager.GetGamePadDisplayName()}", new Vector2(10, 10), Color.White);
 			}
-
+			
 			//_spriteBatch.DrawString(_armadaFont, $" RenderDest.Width: {_renderDestination.Width} RenderDest.Height: {_renderDestination.Height}", new Vector2(10, 10), Color.White);
 			//_spriteBatch.DrawString(_armadaFont, $" PreferredB.Width: {_graphics.PreferredBackBufferWidth} PreferredB.Height: {_graphics.PreferredBackBufferHeight}", new Vector2(10, 30), Color.White);
 
